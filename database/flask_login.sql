e2039f6423a37f26ead0b6b5313282262c3f82fb
AlejandroG <alex.lags@outlook.es>
2022-08-25 13:14:21 -0600

    Implementación de funciones
    Funciones de login y conexión a la base de datos entre otras más.

---
+++ database/flask_login.sql
@@ -0,0 +1,48 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `flask_login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` smallint(3) UNSIGNED NOT NULL,
+  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `password` char(102) COLLATE utf8_unicode_ci NOT NULL,
+  `fullname` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Stores the user''s data.';
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `fullname`) VALUES
+(1, 'ALEJAGARCIA', 'pbkdf2:sha256:260000$I9jPRZ8a6WDJU10q$fcb12b3fd29d732c76ab60bb96ee34215b8b6cd14bd8a987043140ec00856039', 'Alejandro Garcia');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` smallint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
